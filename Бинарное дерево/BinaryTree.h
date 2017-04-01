e64629434caf0a18c68110c47104da2b4198a70a
kate-lozovaya <katekalozovaya@gmail.com>
2017-04-01 22:01:18 +0300

    Update BinaryTree.h

--- Бинарное дерево/BinaryTree.h
+++ Бинарное дерево/BinaryTree.h
@@ -16,9 +16,11 @@ private:
 	Node* root;
 public:
 	Tree();
-	void add(Node* root, Node* newEl);
-	void search(Node* node, int x);
+	Tree(Node* node);
+	Tree(const Tree &node);
+	void add(Node* newEl);
+	Tree search(int x);
 	void fIn(string filename);
-	void fOut(Node* node)const;
-	void Out(Node* node)const;
+	void fOut(string filename)const;
+	void Out(Node* curEl)const;
 };
